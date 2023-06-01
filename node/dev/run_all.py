51ed16a340bb6bde2a68e3e7b47e6e69543a76ec
brommbromley <134511284+brommbromley@users.noreply.github.com>
2023-06-01 15:47:51 +0330

    Create run_all.py

---
+++ node/dev/run_all.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.call(['python', 'us.py'])
+subprocess.call(['python', 'ca.py'])
+subprocess.call(['python', 'nl.py'])
+subprocess.call(['python', 'fr.py'])
+subprocess.call(['python', 'de.py'])
+subprocess.call(['python', 'gb.py'])
