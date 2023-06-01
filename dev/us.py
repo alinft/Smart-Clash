793d96691a6bf58c823650b0a1ea24afc12f2d35
brommbromley <134511284+brommbromley@users.noreply.github.com>
2023-06-01 12:31:36 +0330

    Update

--- dev/us.py
+++ dev/us.py
@@ -1,13 +1,20 @@
 import urllib.request
 import re
+import time
 
 url = "https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge_yaml.yml"
 output_file = "usfiltered.yml"
+six_hours = 6 * 60 * 60  # 6 hours in seconds
 
-with urllib.request.urlopen(url) as f:
-  content = f.read().decode('utf-8')
+while True:
+  current_time = time.time()
+  next_time = current_time + six_hours - (current_time % six_hours)
+  time.sleep(next_time - current_time)
 
-filtered_content = re.sub(r'\n- 🇺🇸US', '', content)
+  with urllib.request.urlopen(url) as f:
+    content = f.read().decode('utf-8')
 
-with open(output_file, "w") as f:
-  f.write(filtered_content)
+  filtered_content = re.sub(r'\n- 🇺🇸US', '', content)
+
+  with open(output_file, "w") as f:
+    f.write("proxies:\n" + filtered_content)
