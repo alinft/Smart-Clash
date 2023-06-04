d2b0cd03a50fe7e7025b233011532cd39a9b3710
brommbromley <134511284+brommbromley@users.noreply.github.com>
2023-06-04 11:48:59 +0330

    ADD!

---
+++ node/dev/ch.py
@@ -0,0 +1,17 @@
+import urllib.request
+import re
+
+url = "https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge_yaml.yml"
+output_file = "./node/ch.yml"
+
+def export_us_content():
+    with urllib.request.urlopen(url) as f:
+        content = f.read().decode('utf-8')
+    filtered_content = re.findall(r'^.*🇨🇭CH.*', content, flags=re.MULTILINE)
+    with open(output_file, "w") as f:
+        f.write("proxies:\n")  # Add proxies: at the beginning of the file
+        for line in filtered_content:
+            f.write(line + "\n")
+    print("🇨🇭CH exported.")
+
+export_us_content()
---
+++ node/dev/ee.py
@@ -0,0 +1,17 @@
+import urllib.request
+import re
+
+url = "https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge_yaml.yml"
+output_file = "./node/ee.yml"
+
+def export_us_content():
+    with urllib.request.urlopen(url) as f:
+        content = f.read().decode('utf-8')
+    filtered_content = re.findall(r'^.*🇪🇪EE.*', content, flags=re.MULTILINE)
+    with open(output_file, "w") as f:
+        f.write("proxies:\n")  # Add proxies: at the beginning of the file
+        for line in filtered_content:
+            f.write(line + "\n")
+    print("🇪🇪EE exported.")
+
+export_us_content()
